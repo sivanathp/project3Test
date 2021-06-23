9fce2570757c33478141e66db453473821889136
Sivanath Peddakotla <speddak1@my.westga.edu>
2021-06-23 08:28:41 -0400

    added routing related tests

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -5,6 +5,8 @@ import { browser, logging , by, element} from 'protractor';
 describe('workspace-project App', () => {
   let page: AppPage;
   var subpageTitle = element(by.id('subpageTitle'));
+  var registrationLink =  element(by.id('registration-navbar'));
+  var bracketsLink =  element(by.id('brackets-navbar'));
 
   beforeEach(() => {
     page = new AppPage();
@@ -19,6 +21,47 @@ describe('workspace-project App', () => {
     page.navigateTo();
     browser.get('/');
     expect(subpageTitle.getText()).toEqual('Brackets App');
+        
+   });
+  
+  it('testing routes', () => {
+    browser.sleep(2000).then(function(){
+      checkNavbarTexts();
+      
+    });
+  }); 
+
+  function checkNavbarTexts(){
+    element(by.id('hello-navbar')).getText().then(function(text){ // Promise
+      expect(text).toEqual('Welcome');
+    });
+
+    element(by.id('registration-navbar')).getText().then(function(text){ // Promise
+      expect(text).toEqual('Registration');
+    });
+
+    element(by.id('brackets-navbar')).getText().then(function(text){ // Promise
+      expect(text).toEqual('Brackets');
+    });
+  }
+
+  it('should go to Registration page', function() {
+    page.navigateTo();
+    browser.get('/');
+    registrationLink.click();
+    
+    expect(element(by.css('h2')).getText()).toEqual('Register Players');
+    
+        
+   });
+
+   it('should go to Brackets page', function() {
+    page.navigateTo();
+    browser.get('/');
+    bracketsLink.click();
+    
+    expect(element(by.css('h2')).getText()).toEqual('Brackets');
+    
         
    });
 
